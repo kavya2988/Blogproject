83bfb2c0e35e6d5897e40e553bce680a50098d54
kavya2988 <kav.g88@gmail.com>
2026-07-17 21:24:54 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./components/Home";
 import Blogs from "./components/Blogs";
-import Navbar from "./components/Common/Navbar";
+import Navbar from "./components/common/Navbar";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import About from "./components/About";
+import Footer from "./components/common/Footer";
 function App() {
   return (
    <div className="px-10  bg-white border rounded-md">
